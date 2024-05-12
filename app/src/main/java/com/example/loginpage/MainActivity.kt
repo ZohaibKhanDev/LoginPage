d66366e6192868ca8c12eb4a349e8ab61a49a784
Muhammad Khubaib Imtiaz <98816544+KhubaibKhan4@users.noreply.github.com>
2024-05-12 10:21:56 +0500

    SignUp and Login page Impl

--- app/src/main/java/com/example/loginpage/MainActivity.kt
+++ app/src/main/java/com/example/loginpage/MainActivity.kt
@@ -10,6 +10,7 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Email
 import androidx.compose.material3.Icon
@@ -33,7 +34,7 @@ class MainActivity : ComponentActivity() {
                     verticalArrangement = Arrangement.Center,
                     horizontalAlignment = Alignment.CenterHorizontally
                 ) {
-                    Icon(imageVector = Icons.Default.Email, contentDescription = "")
+                    Icon(imageVector = Icons.Default.Email, contentDescription = "", modifier = Modifier.size(30.dp))
                     Spacer(modifier = Modifier.height(20.dp))
                     
                     Text(text = "Welcome to Main Screen")
